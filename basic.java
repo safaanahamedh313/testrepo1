271bedacfb2a07691c3e5171c8042fbedb95fe1a
safaan0313 <safaanahamedh313@gmail.com>
2022-11-04 18:46:07 +0530

    basic.java

---
+++ basic.java
@@ -0,0 +1,10 @@
+Public class Basic(){
+
+public static void main (args[]){
+
+	int x=123;
+	int y=124;
+
+	if(x>y){System.out.prinln("iam great"" ""said x");}
+	else   {System.out.println("no iam great");}
+}}
